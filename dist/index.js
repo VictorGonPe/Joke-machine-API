c727c84634012622f7c0f6c155c779ad0790a266
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-26 12:13:24 +0100

    Add API call

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,29 @@
 "use strict";
-fetch('https://icanhazdadjoke.com/', {
-    headers: { Accept: 'application/json' }
-})
-    .then(response => response.json())
-    .then(data => console.log("Joke: ", data.joke));
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const callAPI = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield fetch('https://icanhazdadjoke.com/', {
+            headers: { Accept: 'application/json' }
+        });
+        const data = yield response.json();
+        const p = document.getElementById('joke');
+        if (p)
+            p.innerText = data.joke;
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+callAPI();
+const nextButton = document.querySelector('.next-joke');
+nextButton === null || nextButton === void 0 ? void 0 : nextButton.addEventListener('click', () => {
+    callAPI();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-fetch('https://icanhazdadjoke.com/', {
-    headers: { Accept: 'application/json' }
-  })
-    .then(response => response.json())
-    .then(data => console.log("Joke: ", data.joke));
-  
+const callAPI = async () => {
+  try {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+      headers: { Accept: 'application/json' }
+    })
+    const data = await response.json();
+    
+    const p = document.getElementById('joke');
+    if (p) p.innerText = data.joke;
+
+  }catch (error) {
+    console.log(error);
+  }
+}
+
+callAPI();
+
+const nextButton = document.querySelector('.next-joke');
+nextButton?.addEventListener('click', () => {
+  callAPI(); 
+});
+
+ 
